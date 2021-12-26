56344ede6aed0f3a69d0b5cb08abbc1ba26581ec
ardihp <ardihp8@gmail.com>
2021-12-26 21:39:43 +0700

    refactor: move pages and styles from root folder to src folder

---
+++ src/pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+            integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
+            crossOrigin="anonymous"
+          />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Index() {
+  return (
+    <div className="text-red-500">
+      <i className="fas fa-users" />
+    </div>
+  );
+}
+
+export default Index;
