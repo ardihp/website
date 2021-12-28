2b3f1f08cfe4b724c2c6a83e8c477aac9d37207d
ardihp <ardihp8@gmail.com>
2021-12-28 16:56:25 +0700

    feat: add font from fonts.google.com to tailwind config

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,6 +11,16 @@ class MyDocument extends Document {
             integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
             crossOrigin="anonymous"
           />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Manrope:wght@300;400;500;600;700;800&display=swap"
+            rel="stylesheet"
+          />
         </Head>
 
         <body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      fredoka: "Fredoka One, cursive",
+      manrope: "Manrope, sans-serif",
+    },
     extend: {},
   },
   plugins: [],
