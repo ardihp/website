db6bc595285dca02d2d215314ac8e9012541b694
ardihp <ardihp8@gmail.com>
2021-12-31 11:53:52 +0700

    feat: setup footer component on layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen max-w-full">
       <Navbar />
-      <div className="grow px-6 lg:px-24 duration-300">{children}</div>
+      <div className="flex grow px-6 lg:px-40 duration-300">{children}</div>
+      <Footer />
     </div>
   );
 }
