e793c7f14789bb90a9df81925195e2751f91b6e0
ardihp <ardihp8@gmail.com>
2021-12-31 11:50:59 +0700

    feat: footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="flex justify-center">
+      <div className="flex py-3">
+        <p className="font-manrope text-xs  font-bold text-gray-500/60">
+          MIT License © 2021 Ardiansyah H.P.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
