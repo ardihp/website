06c0eda3f244c7b58d9211ea76d6c7bf23b12c79
ardihp <ardihp8@gmail.com>
2021-12-31 12:08:22 +0700

    style: fix padding size on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -46,8 +46,8 @@ function Header() {
   };
 
   return (
-    <div className="duration-300 sticky shadow-lg shadow-sky-200/25 top-0 border-b-2 border-dashed border-sky-200 rounded-b-xl">
-      <div className="flex justify-between items-center bg-white/80 backdrop-blur-sm py-4 px-6 lg:px-24 duration-300">
+    <div className="duration-300 sticky shadow-lg shadow-sky-200/25 top-0 border-b-2 border-dashed border-sky-200 rounded-b-xl z-50">
+      <div className="flex justify-between items-center bg-white/80 backdrop-blur-sm py-4 px-6 lg:px-40 duration-300">
         <Link href="/" passHref>
           <div className="flex items-center text-sky-500/50 cursor-pointer hover:animate-pulse">
             <i className="fas fa-hat-wizard text-xl mr-1" />
@@ -73,10 +73,15 @@ function Header() {
                   leaveTo="transform opacity-0 scale-95 -translate-y-5"
                 >
                   <Menu.Items
-                    className="flex flex-col items-center bg-white p-4 sm:p-0 border-2 sm:border-0 border-dashed border-sky-300/50 rounded-xl sm:flex-row space-y-4 sm:space-y-0 sm:space-x-6 divide-y sm:divide-y-0 sm:divide-x"
+                    className="flex flex-col items-center bg-white/90 p-4 sm:p-0 border-2 sm:border-0 border-dashed border-sky-300/50 rounded-xl sm:flex-row space-y-4 sm:space-y-0 sm:space-x-6 divide-y sm:divide-y-0 sm:divide-x"
                     static
                   >
                     <div className="flex flex-col items-center space-y-2 sm:space-y-0 sm:flex-row sm:space-x-6 text-sm font-manrope font-bold w-full">
+                      {windowWidth < 640 && (
+                        <Menu.Item>
+                          <ActiveLink href="/">Home</ActiveLink>
+                        </Menu.Item>
+                      )}
                       <Menu.Item>
                         <ActiveLink href="/blog">Blog</ActiveLink>
                       </Menu.Item>
