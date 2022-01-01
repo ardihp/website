6d2f297be01dab52e8e116e2bb85b3446d344c27
ardihp <ardihp8@gmail.com>
2022-01-01 17:23:50 +0700

    style: add background grid on layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 function Layout({ children }) {
+  const [value, setValue] = useState(0);
+  const [grid, setGrid] = useState([]);
+
+  useEffect(() => {
+    if (value < 30) {
+      setGrid([...grid, grid]);
+      setValue(value + 1);
+    } else {
+      console.log("stop");
+    }
+  }, [value, grid]);
+
   return (
-    <div className="flex flex-col min-h-screen max-w-full">
-      <Navbar />
-      <div className="flex grow px-6 lg:px-40 duration-300">{children}</div>
-      <Footer />
+    <div className="flex flex-col max-w-full relative">
+      <div className="flex flex-col relative min-h-screen z-20">
+        <Navbar />
+        <div className="flex grow px-6 lg:px-40 duration-300">{children}</div>
+        <Footer />
+      </div>
+      <div className="absolute top-0 bg-gradient-to-b from-white to-white/50 min-h-screen w-full z-10" />
+      <div className="absolute bottom-0 w-full h-auto z-0">
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-2">
+          {grid.map((g, index) => (
+            <div
+              key={index}
+              className="grid grid-cols-3 sm:grid-cols-6 lg:grid-cols-10"
+            >
+              {grid.map((g, index) => (
+                <div key={index} className="border border-gray-500/10 h-16">
+                  {""}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
