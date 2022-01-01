640b38fb6f1469de5e222b26262201fd24512a29
ardihp <ardihp8@gmail.com>
2022-01-01 23:44:47 +0700

    feat: add SocialLink component

---
+++ src/components/SocialLink.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+
+function SocialLink({ icon, body, href }) {
+  return (
+    <Link href={href}>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="flex items-center justify-center text-sm text-white bg-sky-500/50 space-x-2 duration-300 py-2 px-3 rounded-lg transform hover:-translate-y-1"
+      >
+        <i className={icon} />
+        <p className="font-manrope font-bold">{body}</p>
+      </a>
+    </Link>
+  );
+}
+
+export default SocialLink;
