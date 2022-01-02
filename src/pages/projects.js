29bdd2c9524683cb7d002830e92b07959075eb21
ardihp <ardihp8@gmail.com>
2022-01-02 12:37:29 +0700

    feat: add project page

---
+++ src/pages/projects.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Projects() {
+  Projects.title = "Project's - Ardi";
+
+  return (
+    <div>
+      <p>ini halaman projects</p>
+    </div>
+  );
+}
+
+export default Projects;
