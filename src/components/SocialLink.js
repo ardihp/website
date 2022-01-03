a08372d76d015ddd6a13654b25431d37b10edaa8
ardihp <ardihp8@gmail.com>
2022-01-03 11:32:18 +0700

    style: fix padding size on sociallink component

--- src/components/SocialLink.js
+++ src/components/SocialLink.js
@@ -7,7 +7,7 @@ function SocialLink({ icon, body, href }) {
       <a
         target="_blank"
         rel="noreferrer"
-        className="flex items-center justify-center text-sm text-white bg-sky-500/50 space-x-2 duration-300 py-2 px-3 rounded-lg transform shadow-lg shadow-sky-400/30 hover:-translate-y-1"
+        className="flex items-center justify-center text-sm text-white bg-sky-500/50 space-x-2 duration-300 p-3 rounded-lg transform shadow-lg shadow-sky-400/30 hover:-translate-y-1"
       >
         <i className={icon} />
         <p className="font-manrope font-bold">{body}</p>
