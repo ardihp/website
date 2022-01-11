2f5bacde722bc6e0cc96f74eb7a1a23da25dd796
ardihp <ardihp8@gmail.com>
2022-01-11 15:43:17 +0700

    style: update blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { getBlog } from "@/lib/getBlog";
+import BlogCard from "@/components/BlogCard";
 
+Blog.title = "Blog's - Ardi";
 function Blog({ blogs }) {
-  console.log(blogs);
-
   return (
-    <div>
-      <p>ini halaman blog</p>
+    <div className="flex flex-col py-6 space-y-6 w-full">
+      <div className="flex flex-col space-y-2">
+        <p className="font-fredoka text-4xl lg:text-5xl text-sky-500/50">
+          Blogs
+        </p>
+        <p className="font-manrope font-bold text-gray-600/70">
+          Place for me to share something using words.
+        </p>
+      </div>
+      <div className="grid md:grid-cols-2 gap-6">
+        {blogs.map((blog, i) => (
+          <BlogCard key={i} {...blog} />
+        ))}
+      </div>
     </div>
   );
 }
