05dca524ee30f56c4356d0b2feb2220b822e3426
ardihp <ardihp8@gmail.com>
2022-01-11 15:44:01 +0700

    feat: add page for blog by slug

---
+++ src/pages/blog/[slug].js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+function BlogSlug() {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  return <div>{slug}</div>;
+}
+
+export default BlogSlug;
