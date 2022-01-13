b68e61c27b2d7619809668cc4a1a849b9a50e573
ardihp <ardihp8@gmail.com>
2022-01-13 21:34:34 +0700

    style: fix optimization blog card

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -7,8 +7,8 @@ function BlogCard({ slug, body }) {
 
   return (
     <Link href={`/blog/` + slug}>
-      <a className="flex transform duration-300 hover:-translate-y-1 active:translate-y-0 active:scale-95">
-        <div className="flex flex-col border-2 p-3 border-dashed border-gray-300/30 rounded-lg bg-white/10 backdrop-blur-sm">
+      <a className="transform duration-300 border-dashed border-2 border-gray-300/30 rounded-lg hover:-translate-y-1 active:translate-y-0 active:scale-95">
+        <div className="flex flex-col h-full p-3 bg-white/10 backdrop-blur-sm">
           <div className="flex bg-gray-50 rounded-lg overflow-hidden relative mb-2">
             <div className="absolute top-2 left-2 z-10">
               <div className="flex items-center bg-gray-700/60 px-2 py-1 rounded-md">
@@ -17,7 +17,7 @@ function BlogCard({ slug, body }) {
                 </p>
               </div>
             </div>
-            <div className="h-auto w-full relative">
+            <div className="w-full">
               <Image
                 src={image}
                 alt={title}
@@ -27,10 +27,10 @@ function BlogCard({ slug, body }) {
               />
             </div>
           </div>
-          <p className="font-fredoka text-xl hover:underline underline-offset-1 text-gray-600/60 mb-1">
+          <p className="font-fredoka text-xl hover:underline underline-offset-1 text-gray-600/60">
             {title}
           </p>
-          <p className="font-manrope text-gray-600/60 font-bold text-sm mb-auto">
+          <p className="font-manrope text-gray-600/60 font-bold text-sm mb-auto mt-1">
             {excerpt}
           </p>
         </div>
