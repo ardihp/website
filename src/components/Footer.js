193b0d6d8156ca41e9a20afb98d7ea82f389daee
ardihp <ardihp8@gmail.com>
2022-01-15 14:33:31 +0700

    style: add dark mode adjustment on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="flex justify-center">
       <div className="flex py-3">
-        <p className="font-manrope text-xs  font-bold text-gray-500/60">
+        <p className="font-manrope text-xs  font-bold dark:text-slate-400 text-gray-500/60">
           MIT License © 2021 Ardiansyah H.P.
         </p>
       </div>
