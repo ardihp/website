2f2ddba9c31c5e53789ce8f7474403c99759e66d
ardihp <ardihp8@gmail.com>
2022-01-15 22:50:33 +0700

    style: change ipad display on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,17 +7,17 @@ Index.title = "Ardi's Personal Website";
 
 function Index() {
   return (
-    <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 w-full justify-center items-center">
-      <div className="flex flex-col items-center md:items-start space-y-8 md:w-full duration-300">
-        <div className="flex flex-col items-center md:items-start space-y-3 w-full">
-          <p className="font-fredoka text-4xl md:text-5xl dark:text-white text-sky-500/50">
-            <span className="dark:bg-slate-700/60 bg-sky-500/50 rounded-md text-white mr-2 px-2 tracking-wider">
+    <div className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 w-full justify-center items-center">
+      <div className="flex flex-col items-center lg:items-start space-y-8 md:w-full duration-300">
+        <div className="flex flex-col items-center lg:items-start space-y-3 w-full">
+          <p className="font-fredoka text-4xl sm:text-5xl dark:text-white text-sky-500/50">
+            <span className="dark:bg-slate-700/60 bg-sky-500/50 rounded-lg text-white mr-2 px-2 tracking-wider">
               Hai
             </span>
             , I&apos;m Ardi
             <i className="fas fa-paw ml-4 transform rotate-12" />
           </p>
-          <p className="font-manrope font-bold text-md dark:text-slate-400 text-gray-600/80 text-center md:text-left w-72 md:w-96">
+          <p className="font-manrope font-bold text-lg dark:text-slate-400 text-gray-600/80 text-center lg:text-left w-72 sm:w-96">
             On my way to becoming a software engineer specialized in frontend
             development.
           </p>
@@ -30,10 +30,10 @@ function Index() {
         </Link>
       </div>
       <div
-        className="flex justify-center overflow-hidden items-center rounded-full duration-300 md:w-1/2"
+        className="flex justify-center overflow-hidden items-center rounded-full duration-300 w-64 lg:w-1/2"
         title="me lazy asf :)"
       >
-        <div className="w-64 md:w-full">
+        <div className="w-full">
           <Image
             src={IndexPict}
             alt="gambar index"
