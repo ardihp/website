b26fe145d6ff21c0db05d4ecb6e6fca02b8b176c
ardihp <ardihp8@gmail.com>
2022-01-15 22:25:58 +0700

    style: add dark mode adjustment on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,20 +7,23 @@ About.title = "About - Ardi";
 function About() {
   return (
     <div className="flex flex-col py-6 space-y-6 lg:space-y-8 w-full">
-      <p className="font-fredoka text-4xl lg:text-5xl sm:w-2/3 md:w-7/12 text-sky-500/50">
+      <p className="font-fredoka text-4xl lg:text-5xl sm:w-2/3 md:w-7/12 dark:text-slate-200 text-sky-500/50">
         Hey, it&apos;s a nice to meet you here {""}
         <span className="font-normal">⛄️</span>
       </p>
       <div className="flex flex-col space-y-3">
-        <p className="font-fredoka text-xl md:text-2xl text-sky-500/50">
+        <p className="font-fredoka text-xl md:text-2xl dark:text-slate-200 text-sky-500/50">
           About Me
         </p>
         <div className="flex flex-col space-y-6">
-          <div className="font-manrope font-bold text-gray-600/70 space-y-3 leading-loose">
+          <div className="font-manrope font-bold dark:text-slate-400 text-gray-600/70 space-y-3 leading-loose">
             <p>
               My name is{" "}
-              <span className="text-gray-800">Ardiansyah Halim Putra</span>,
-              people usualy call me <span className="text-gray-800">Ardi</span>.
+              <span className="dark:text-slate-200 text-gray-800">
+                Ardiansyah Halim Putra
+              </span>
+              , people call me{" "}
+              <span className="dark:text-slate-200 text-gray-800">Ardi</span>.
               Born and rise in{" "}
               <LinkHref
                 body="Jakarta, Indonesia"
@@ -40,14 +43,18 @@ function About() {
                 target="_blank"
               />{" "}
               Vocational High School majoring{" "}
-              <span className="text-gray-800">Software Engineering</span> last
-              year, 2021.
+              <span className="dark:text-slate-200 text-gray-800">
+                Software Engineering
+              </span>{" "}
+              last year, 2021.
             </p>
             <p>
               I like things related to{" "}
-              <span className="text-gray-800">design and technology</span>,
-              including {""}
-              <span className="text-gray-800">
+              <span className="dark:text-slate-200 text-gray-800">
+                design and technology
+              </span>
+              , including {""}
+              <span className="dark:text-slate-200 text-gray-800">
                 interaction design, accessibility, and technology design
               </span>
               . I started learning web development since 1st year at school
@@ -75,7 +82,7 @@ function About() {
             </p>
           </div>
           <div className="flex flex-col space-y-4">
-            <p className="font-manrope font-bold text-gray-600/70">
+            <p className="font-manrope font-bold dark:text-slate-200 text-gray-600/70">
               Feel free to get in touch with me via links below:
             </p>
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-flow-col md:auto-cols-fr gap-4 w-full">
