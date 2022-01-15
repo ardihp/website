d7068d5ef6fbc7ed82cd3773cc6b0fd2e8929fe8
ardihp <ardihp8@gmail.com>
2022-01-15 15:09:52 +0700

    feat: add cta project button on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Image from "next/image";
 import IndexPict from "@/public/index.png";
-import LinkHref from "@/components/LinkHref";
+import Link from "next/link";
 
 Index.title = "Ardi's Personal Website";
 
 function Index() {
   return (
     <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 w-full justify-center items-center">
-      <div className="flex flex-col items-center md:items-start space-y-6 md:w-full duration-300">
+      <div className="flex flex-col items-center md:items-start space-y-8 md:w-full duration-300">
         <div className="flex flex-col items-center md:items-start space-y-3 w-full">
           <p className="font-fredoka text-4xl md:text-5xl dark:text-white text-sky-500/50">
-            <span className="dark:bg-sky-400/90 bg-sky-500/50 rounded-md text-white mr-2 px-2 tracking-wider">
+            <span className="dark:bg-slate-700/60 bg-sky-500/50 rounded-md text-white mr-2 px-2 tracking-wider">
               Hai
             </span>
             , I&apos;m Ardi
@@ -22,10 +22,12 @@ function Index() {
             development.
           </p>
         </div>
-        <p className="font-manrope font-bold text-md dark:text-slate-400 text-gray-600/80">
-          Wanna see my past work ? {""}
-          <LinkHref body="here" href="/projects" />.
-        </p>
+        <Link href="/projects">
+          <a className="flex dark:bg-sky-500 dark:hover:bg-sky-400 bg-slate-400/70 hover:bg-slate-400/50 h-10 px-6 shadow-lg dark:shadow-sky-500/25 rounded-lg space-x-2 items-center text-white duration-300">
+            <p className="font-manrope font-bold text-sm">See my past work</p>
+            <i className="fas fa-angle-right" />
+          </a>
+        </Link>
       </div>
       <div
         className="flex justify-center overflow-hidden items-center rounded-full duration-300 md:w-1/2"
