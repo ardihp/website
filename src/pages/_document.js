d91f1619e3586fbd43f3961c65435971921d66c7
ardihp <ardihp8@gmail.com>
2022-01-18 14:54:59 +0700

    feat: add favicon image

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -21,6 +21,24 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Manrope:wght@300;400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicon/manifest.json" />
         </Head>
 
         <body>
