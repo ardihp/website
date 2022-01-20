36bccb8adbcf5804c6e76fdcffe42786823fc485
ardihp <ardihp8@gmail.com>
2022-01-20 23:30:17 +0700

    refactor: social link data

---
+++ src/data/social.js
@@ -0,0 +1,29 @@
+const social = [
+  {
+    href: "mailto:ardihp8@gmail.com",
+    icon: "far fa-envelope",
+    body: "Email",
+  },
+  {
+    href: "https://linkedin.com/in/ardi-hp/",
+    icon: "fab fa-linkedin-in",
+    body: "LinkedIn",
+  },
+  {
+    href: "https://github.com/ardihp",
+    icon: "fab fa-github-alt",
+    body: "Github",
+  },
+  {
+    href: "https://medium.com/@ardihp8",
+    icon: "fab fa-medium-m",
+    body: "Medium",
+  },
+  {
+    href: "https://www.instagram.com/ardi.hp/",
+    icon: "fab fa-instagram",
+    body: "Instagram",
+  },
+];
+
+export default social;
