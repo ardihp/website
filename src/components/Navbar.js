905b566c995c15f6c5f2989a375e5aab791401d7
ardihp <ardihp8@gmail.com>
2022-01-20 20:22:14 +0700

    feat: setup mobilemenu component on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Link from "next/link";
 import ActiveLink from "@/components/ActiveLink";
-import { Menu, Transition } from "@headlessui/react";
+import { Menu } from "@headlessui/react";
 import ThemeChanger from "./ThemeChanger";
+import MobileMenu from "./MobileMenu";
 
 function Header() {
-  const [windowWidth, setWindowWidth] = useState();
-
-  useEffect(() => {
-    setWindowWidth(window.screen.width);
-  }, [windowWidth]);
-
   return (
     <div className="flex max-w-5xl mx-auto justify-between items-center py-4 px-6 duration-300">
       <Link href="/" passHref>
@@ -18,59 +13,33 @@ function Header() {
           <p className="font-korean text-4xl font-bold">아르디</p>
         </div>
       </Link>
-      <Menu as="div" className="flex relative w-full justify-end">
-        {({ open }) => (
-          <>
-            <div className="sm:hidden">
-              <Menu.Button className="flex items-center justify-center w-9 h-9 dark:hover:bg-sky-500/30 hover:bg-sky-300/50 rounded-full dark:text-white text-sky-300 hover:text-white duration-300">
-                <i className="fas fa-bars text-lg" />
-              </Menu.Button>
-            </div>
-            <div className="absolute top-16 right-2 sm:relative sm:inline-block sm:top-0 w-48 sm:w-auto">
-              <Transition
-                show={windowWidth >= 640 ? true : open}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95 -translate-y-5"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95 -translate-y-5"
+      <div className="sm:hidden">
+        <MobileMenu />
+      </div>
+      <div className="hidden sm:inline-block">
+        <Menu
+          as="div"
+          className="flex items-center dark:bg-slate-900/90 dark:sm:bg-transparent bg-transparent sm:space-x-6 sm:divide-x"
+        >
+          <div className="flex items-center space-x-6 text-sm font-manrope font-bold w-full">
+            <ActiveLink href="/blog">Blog</ActiveLink>
+            <ActiveLink href="/projects">Projects</ActiveLink>
+            <ActiveLink href="/about">About</ActiveLink>
+          </div>
+          <div className="flex space-x-4 sm:pl-6 justify-between items-center pt-4 sm:pt-0 w-full">
+            <ThemeChanger />
+            <div className="flex items-center justify-center w-9 h-9 dark:text-white text-gray-400 dark:hover:text-opacity-50 hover:text-opacity-50 rounded-full duration-300">
+              <a
+                href="https://github.com/ardihp/website"
+                target="_blank"
+                rel="noreferrer"
               >
-                <Menu.Items
-                  className="flex flex-col items-center dark:bg-slate-900/90 dark:sm:bg-transparent bg-white/90 sm:bg-transparent p-4 sm:p-0 border-2 sm:border-0 border-dashed dark:border-slate-400 border-sky-300/50 rounded-xl sm:flex-row space-y-4 sm:space-y-0 sm:space-x-6 divide-y sm:divide-y-0 sm:divide-x"
-                  static
-                >
-                  <div className="flex flex-col items-center space-y-2 sm:space-y-0 sm:flex-row sm:space-x-6 text-sm font-manrope font-bold w-full">
-                    <Menu.Item>
-                      <ActiveLink href="/blog">Blog</ActiveLink>
-                    </Menu.Item>
-                    <Menu.Item>
-                      <ActiveLink href="/projects">Projects</ActiveLink>
-                    </Menu.Item>
-                    <Menu.Item>
-                      <ActiveLink href="/about">About</ActiveLink>
-                    </Menu.Item>
-                  </div>
-                  <div className="flex space-x-4 sm:pl-6 justify-between items-center pt-4 sm:pt-0 w-full">
-                    <ThemeChanger />
-                    <div className="flex items-center justify-center w-9 h-9 dark:text-white text-gray-400 dark:hover:text-opacity-50 hover:text-opacity-50 rounded-full duration-300">
-                      <Menu.Item>
-                        <a
-                          href="https://github.com/ardihp/website"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="fab fa-github text-xl" />
-                        </a>
-                      </Menu.Item>
-                    </div>
-                  </div>
-                </Menu.Items>
-              </Transition>
+                <i className="fab fa-github text-xl" />
+              </a>
             </div>
-          </>
-        )}
-      </Menu>
+          </div>
+        </Menu>
+      </div>
     </div>
   );
 }
