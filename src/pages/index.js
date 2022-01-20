96aeb5a4f2ea758cdf3f07ea7aadbfc2a8a16a6b
ardihp <ardihp8@gmail.com>
2022-01-20 23:59:44 +0700

    feat: add enter page animation on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import IndexPict from "@/public/index.png";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 Index.title = "Ardi's Personal Website";
 
@@ -10,37 +11,58 @@ function Index() {
     <div className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 w-full py-8 justify-center items-center">
       <div className="flex flex-col items-center lg:items-start space-y-8 md:w-full duration-300">
         <div className="flex flex-col items-center lg:items-start space-y-3 w-full">
-          <p className="font-fredoka text-4xl sm:text-5xl dark:text-white text-sky-500/50">
+          <motion.p
+            initial={{ y: 20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.3 }}
+            className="font-fredoka text-4xl sm:text-5xl dark:text-white text-sky-500/50"
+          >
             <span className="dark:bg-slate-700/60 bg-sky-500/50 rounded-lg text-white mr-2 px-2 tracking-wider">
               Hai
             </span>
             , I&apos;m Ardi
             <i className="fas fa-paw ml-4 transform rotate-12" />
-          </p>
-          <p className="font-manrope font-bold text-lg dark:text-slate-400 text-gray-600/80 text-center lg:text-left w-72 sm:w-96">
+          </motion.p>
+          <motion.p
+            initial={{ y: 20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.3, delay: 0.15 }}
+            className="font-manrope font-bold text-lg dark:text-slate-400 text-gray-600/80 text-center lg:text-left w-72 sm:w-96"
+          >
             On my way to becoming a software engineer specialized in frontend
             development.
-          </p>
+          </motion.p>
         </div>
-        <Link href="/projects">
-          <a className="flex dark:bg-sky-500 dark:hover:bg-sky-400 bg-slate-400/70 hover:bg-slate-400/50 h-10 px-6 shadow-lg dark:shadow-sky-500/25 rounded-lg space-x-2 items-center text-white duration-300">
-            <p className="font-manrope font-bold text-sm">See my past work</p>
-            <i className="fas fa-angle-right" />
-          </a>
-        </Link>
+        <motion.div
+          initial={{ y: 20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.3, delay: 0.3 }}
+        >
+          <Link href="/projects">
+            <a className="flex dark:bg-sky-500 dark:hover:bg-sky-400 bg-slate-400/70 hover:bg-slate-400/50 h-10 px-6 shadow-lg dark:shadow-sky-500/25 rounded-lg space-x-2 items-center text-white duration-300">
+              <p className="font-manrope font-bold text-sm">See my past work</p>
+              <i className="fas fa-angle-right" />
+            </a>
+          </Link>
+        </motion.div>
       </div>
       <div
         className="flex justify-center overflow-hidden items-center rounded-full duration-300 w-64 lg:w-1/2"
         title="me lazy asf :)"
       >
-        <div className="w-full">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="w-full"
+        >
           <Image
             src={IndexPict}
             alt="gambar index"
             layout="responsive"
             placeholder="blur"
           />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
