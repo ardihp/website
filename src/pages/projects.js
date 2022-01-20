71303746637f1c5919dc51adb72a3a8c3b4c47ab
ardihp <ardihp8@gmail.com>
2022-01-20 23:10:38 +0700

    feat: add enter page animation on projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import projects from "@/data/projects";
 import ProjectCard from "@/components/ProjectCard";
+import { motion } from "framer-motion";
 
 Projects.title = "Project's - Ardi";
 
@@ -8,16 +9,26 @@ function Projects() {
   return (
     <div className="flex flex-col py-6 space-y-6 w-full">
       <div className="flex flex-col space-y-2">
-        <p className="font-fredoka text-4xl lg:text-5xl dark:text-white text-sky-500/50">
+        <motion.p
+          initial={{ y: 20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.4 }}
+          className="font-fredoka text-4xl lg:text-5xl dark:text-white text-sky-500/50"
+        >
           Projects
-        </p>
-        <p className="font-manrope font-bold dark:text-slate-400 text-gray-600/70">
+        </motion.p>
+        <motion.p
+          initial={{ y: 20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.55 }}
+          className="font-manrope font-bold dark:text-slate-400 text-gray-600/70"
+        >
           Place for my projects collection about frontend development.
-        </p>
+        </motion.p>
       </div>
       <div className="grid md:grid-cols-2 gap-6">
         {projects.map((project, i) => (
-          <ProjectCard key={i} {...project} />
+          <ProjectCard key={i} idx={i} {...project} />
         ))}
       </div>
     </div>
