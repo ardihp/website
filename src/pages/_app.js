cac45dc1c883b39fc1b6f9c305edd193137410a1
ardihp <ardihp8@gmail.com>
2022-01-21 15:18:02 +0700

    feat: setup animate presence on _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import Layout from "@/components/Layout";
 import { ThemeProvider } from "next-themes";
 import Head from "next/head";
 import Progress from "@/components/Progress";
+import { AnimatePresence } from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -12,7 +13,9 @@ function MyApp({ Component, pageProps }) {
           <meta content="width=device-width, initial-scale=1" name="viewport" />
           <title>{Component.title}</title>
         </Head>
-        <Component {...pageProps} />
+        <AnimatePresence exitBeforeEnter>
+          <Component {...pageProps} key={Component.route} />
+        </AnimatePresence>
         <Progress />
       </Layout>
     </ThemeProvider>
