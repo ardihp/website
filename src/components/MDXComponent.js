32882f547139ca689c843be36541f2bd93e2c580
ardihp <ardihp8@gmail.com>
2022-01-26 13:50:43 +0700

    style: remove animation on mdx component

--- src/components/MDXComponent.js
+++ src/components/MDXComponent.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import Image from "next/image";
-import { motion } from "framer-motion";
 
 function getAnchor(text) {
   return text
@@ -11,14 +10,9 @@ function getAnchor(text) {
 
 export const Paragraph = ({ children }) => {
   return (
-    <motion.p
-      whileInView={{ opacity: [0, 1], y: [20, 0] }}
-      viewport={{ once: true }}
-      transition={{ duration: 0.3 }}
-      className="font-manrope font-bold dark:text-slate-400 text-gray-600/70"
-    >
+    <p className="font-manrope font-bold dark:text-slate-400 text-gray-600/70">
       {children}
-    </motion.p>
+    </p>
   );
 };
 
@@ -26,19 +20,13 @@ export const H2 = ({ children }) => {
   const anchor = getAnchor(children);
   const link = `#${anchor}`;
   return (
-    <motion.h2
-      whileInView={{ opacity: [0, 1], y: [20, 0] }}
-      viewport={{ once: true }}
-      transition={{ duration: 0.3 }}
-      id={anchor}
-      className="flex font-fredoka mt-6 mb-4 scroll-my-24"
-    >
+    <h2 id={anchor} className="flex font-fredoka mt-6 mb-4 scroll-my-24">
       <Link href={link}>
         <a className="dark:text-slate-200 text-gray-600/80 no-underline hover:underline underline-offset-1">
           {children}
         </a>
       </Link>
-    </motion.h2>
+    </h2>
   );
 };
 
