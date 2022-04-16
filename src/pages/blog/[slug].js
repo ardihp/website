7cad986ce566e4c56cfa27f75abbf27d0febf9a0
ardihp <ardihp8@gmail.com>
2022-04-16 21:08:58 +0700

    fix: blog slug page animation

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -13,60 +13,36 @@ BlogPage.route = "slug";
 function BlogPage({ body: { title, date, excerpt }, mdxSource }) {
   return (
     <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.3 }}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.15 }}
       className="flex space-x-8 pt-8 pb-6 w-full"
     >
       <Head>
         <title>{title} - Blog&apos;s Ardi</title>
       </Head>
       <div className="flex flex-col space-y-4 w-full lg:w-9/12">
-        <motion.div
-          animate={{ opacity: [0, 1], y: [20, 0] }}
-          exit={{ opacity: 0, y: 20 }}
-          transition={{ duration: 0.3 }}
-        >
+        <div>
           <div className="inline-flex dark:bg-slate-700/60 bg-sky-500/50 h-8 px-4 rounded-full items-center">
             <p className="font-manrope font-bold text-white text-xs">{date}</p>
           </div>
-        </motion.div>
-        <div className="flex flex-col space-y-2">
-          <motion.p
-            animate={{ opacity: [0, 1], y: [20, 0] }}
-            exit={{ opacity: 0, y: 20 }}
-            transition={{ duration: 0.3, delay: 0.15 }}
-            className="font-fredoka text-4xl lg:text-5xl dark:text-slate-200 text-sky-500/50"
-          >
-            {title}
-          </motion.p>
-          <motion.p
-            animate={{ opacity: [0, 1], y: [20, 0] }}
-            exit={{ opacity: 0, y: 20 }}
-            transition={{ duration: 0.3, delay: 0.3 }}
-            className="font-manrope font-bold dark:text-slate-400 text-gray-600/70"
-          >
-            {excerpt}
-          </motion.p>
         </div>
-        <motion.div
-          animate={{ opacity: [0, 1], y: [20, 0] }}
-          exit={{ opacity: 0, y: 20 }}
-          transition={{ duration: 0.3, delay: 0.45 }}
-          className="prose prose-hr:mb-6 prose-hr:mt-0 prose-zinc dark:prose-invert max-w-none"
-        >
+        <div className="flex flex-col space-y-2">
+          <p className="font-fredoka text-4xl lg:text-5xl dark:text-slate-200 text-sky-500/50">
+            {title}
+          </p>
+          <p className="font-manrope font-bold dark:text-slate-400 text-gray-600/70">
+            {excerpt}
+          </p>
+        </div>
+        <div className="prose prose-hr:mb-6 prose-hr:mt-0 prose-zinc dark:prose-invert max-w-none">
           <MDXRemote
             {...mdxSource}
             components={{ p: Paragraph, h2: H2, img: Images }}
           />
-        </motion.div>
+        </div>
       </div>
-      <motion.div
-        animate={{ opacity: [0, 1], y: [-50, 0] }}
-        exit={{ opacity: 0, y: -50 }}
-        transition={{ duration: 0.6 }}
-        className="hidden lg:block lg:w-3/12 relative pl-10"
-      >
+      <div className="hidden lg:block lg:w-3/12 relative pl-10">
         <div className="sticky top-28">
           <div className="flex flex-col space-y-3 border-2 border-dashed dark:border-sky-400/40 dark:bg-sky-500/5 pb-5 rounded-lg">
             <Image src={AuthorImage} alt="gambar author" placeholder="blur" />
@@ -77,7 +53,7 @@ function BlogPage({ body: { title, date, excerpt }, mdxSource }) {
             </div>
           </div>
         </div>
-      </motion.div>
+      </div>
     </motion.div>
   );
 }
