f4b40edcf6b904d1aa36680f50f176aa111f18d7
ardihp <ardihp8@gmail.com>
2024-07-04 22:10:06 +0700

    fix: implement basepath to data assets

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -16,21 +16,21 @@ function MyApp({ Component, pageProps }) {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/favicon/apple-touch-icon.png"
+            href="/v1/favicon/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicon/favicon-32x32.png"
+            href="/v1/favicon/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicon/favicon-16x16.png"
+            href="/v1/favicon/favicon-16x16.png"
           />
-          <link rel="manifest" href="/favicon/manifest.json" />
+          <link rel="manifest" href="/v1/favicon/manifest.json" />
         </Head>
         <AnimatePresence exitBeforeEnter>
           <Component {...pageProps} key={Component.route} />
